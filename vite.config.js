3011e4f43abf429b7cadb1cd6ee21f962cdaf0a6
McG <rmblstrp@users.noreply.github.com>
2023-08-27 14:05:27 -0500

    Switched to using Vue along with Breeze

--- vite.config.js
+++ vite.config.js
@@ -1,17 +1,20 @@
 import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
-import react from '@vitejs/plugin-react';
+import vue from '@vitejs/plugin-vue';
 
 export default defineConfig({
     plugins: [
-        react(),
         laravel({
-            input: ['resources/css/app.css', 'resources/js/app.jsx'],
+            input: 'resources/js/app.js',
             refresh: true,
         }),
+        vue({
+            template: {
+                transformAssetUrls: {
+                    base: null,
+                    includeAbsolute: false,
+                },
+            },
+        }),
     ],
-    server: {
-        host: true,
-        port: 8888
-    }
 });
