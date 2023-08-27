8b6f55915ab02dcde51778bc852baab475793692
McG <rmblstrp@users.noreply.github.com>
2023-08-27 13:50:05 -0500

    Made considerable progress, but am still hampered by the vite server

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Inertia\Inertia;
 
 /*
 |--------------------------------------------------------------------------
@@ -14,5 +15,5 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return Inertia::render('Test'); // This will get component Test.jsx from the resources/js/Pages/Test.jsx
 });
